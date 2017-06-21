9d314d691fb861da9b172074c2e545c78d22b4c6
asafrubin <asafrub@campus.technion.ac.il>
2017-06-21 17:08:07 +0300

    pp

--- list.h
+++ list.h
@@ -1,23 +1,68 @@
 #ifndef HW04_LIST_H
 #define HW04_LIST_H
 
-template <typename T>
-class list{
+#include <cstdio>
+#include <cassert>
 
 
+template <class T, class compare>
+class Node {
+    Node* next;
+    Node* previous;
+    T *data;
+public:
+    // no paramteres constructor
+    Node() : next(NULL), previous(NULL), T(NULL) {};
 
+    Node(const T& data, Node *next, Node *previous) : next(next), previous(previous), data(new T(data))
+    {
+        if(previous) previous->next = this;
+        if(next) next->previous = this;
+    }
 
+    const T& getData() const
+    {
+        assert(this->data != NULL);
+        return *this->data;
+    }
 
+    Node* getNext() const
+    {
+        return this->next;
+    }
 
+    Node* getPrevious() const
+    {
+        return this->previous;
+    }
 
+    void setNext(Node* node)
+    {
+        this->next = node;
+        if(node) node->previous = this;
+    }
 
+    void setPrevious(Node* node)
+    {
+        this->previous = node;
+        if(node) node->next = this;
+    }
 
-
+    void setData(const T& data)
+    {
+        this->data = new T(data);
+    }
 
 
 
 
 };
+
+template <class T>
+class List{
+
+};
+
 
 
 #endif //HW04_LIST_H
